6c2f62c3399f6dc48f5bd8e08236d6206a12d959
Alex Rybachev <rybachev.as@gmail.com>
2022-06-02 20:55:30 +0400

    Initial Commit

---
+++ MyAwardsCollectionApp/MyAwardsCollectionAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAwardsCollectionAppApp.swift
+//  MyAwardsCollectionApp
+//
+//  Created by Aleksandr Rybachev on 02.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct MyAwardsCollectionAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
